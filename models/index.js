17ffe8748adb7ecc96ace12b86bbee016bd249e6
MeganKR <megankaye2018@gmail.com>
2024-02-13 14:02:20 -0600

    work across many files

--- models/index.js
+++ models/index.js
@@ -14,16 +14,18 @@ Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 
-// ???????
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
+  foreignKey: 'product_id'
 });
 
-// ???????
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'product_id',
+  through: ProductTag,
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
